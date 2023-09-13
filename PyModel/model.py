e4f2426ee6f52db0124613303255c30fbd9e54d6
Arnav Shirodkar <66931126+Arnavkar@users.noreply.github.com>
2023-09-13 01:04:57 -0400

    Starting python model, Found of a way of encoding midi data with methods similar to Oore et al 2018

---
+++ PyModel/model.py
@@ -0,0 +1,5 @@
+#Create a new tensorflow model based on transformers
+#the input data will be midi files
+
+import tensorflow as tf
+import numpy as np
---
+++ PyModel/prepare_data.py
@@ -0,0 +1,4 @@
+from DataHandler import DataHandler
+
+handler = DataHandler()
+handler.process_midi_files()
