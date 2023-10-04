90eaa946ce67256825c66bfe807ac7b029f0d836
Arnav Shirodkar <66931126+Arnavkar@users.noreply.github.com>
2023-10-03 22:20:52 -0400

    Added fixes for loading path, appended PyModel path

--- PyModel/Layers/AddNormalizationLayer.py
+++ PyModel/Layers/AddNormalizationLayer.py
@@ -1,3 +1,6 @@
+import sys
+# appending Layer path
+sys.path.append('./PyModel')
 
 from tensorflow.keras.layers import Layer, LayerNormalization
 import tensorflow as tf
@@ -8,8 +11,9 @@ class AddNormalization(Layer):
         super().__init__(**kwargs)
         self.layer_norm = LayerNormalization()  # Layer normalization layer
     
-    def call(self, x, sublayer_x):       
-        return self.layer_norm(x + sublayer_x)
+    def call(self, x, sublayer_x):
+        x += sublayer_x       
+        return self.layer_norm(x)
     
 if __name__ == "__main__":
     test_tensor = tf.constant([[1,3,4,2,0]])
--- PyModel/Layers/FeedForwardLayer.py
+++ PyModel/Layers/FeedForwardLayer.py
@@ -1,15 +1,19 @@
 import sys
 # appending Layer path
-sys.path.append('Layers')
+sys.path.append('./PyModel')
 
-from tensorflow.keras.layers import Layer, Dense
+from tensorflow.keras.layers import Layer, Dense, ReLU
 from Layers.utils import check_shape
     
 class FeedForward(Layer):
     def __init__(self, d_in, d_out, **kwargs):
         super().__init__(**kwargs)
-        self.fully_connected1 = Dense(d_in, activation = "relu")  # First fully connected layer takes in input dimensions
+        self.fully_connected1 = Dense(d_in)  # First fully connected layer takes in input dimensions
         self.fully_connected2 = Dense(d_out)  # Second fully connected layer, based on model output dimensions
+        self.activation = ReLU()
 
     def call(self, x):
-        return self.fully_connected2(self.fully_connected1(x))
+        output = self.fully_connected1(x)
+        output = self.activation(output)
+        output = self.fully_connected2(output)
+        return output
