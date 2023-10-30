b560ec0ba3917ed2c8d7938a89e213c0077b650e
Arnav Shirodkar <66931126+Arnavkar@users.noreply.github.com>
2023-10-30 15:17:07 -0400

    file to test samples

---
+++ PyModel/test_samples.py
@@ -0,0 +1,14 @@
+import pretty_midi
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i',"--input_seq_path", type=str,required= True)
+parser.add_argument('-o',"--output_seq_path", type=str,required= True)
+args = parser.parse_args()
+input_midi = pretty_midi.PrettyMIDI(args.input_seq_path)
+output_midi = pretty_midi.PrettyMIDI()
+
+print(input_midi.get_onsets())
+print(output_midi.get_onsets())
+
+
