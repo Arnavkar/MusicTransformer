76caac2e7c77554389dddb8722ac76e4feee0492
Arnav Shirodkar <66931126+Arnavkar@users.noreply.github.com>
2023-11-25 19:12:05 -0500

    Refactored Customdataset to now have a baseclass dataset, to build different datasets and test pipeline efficiency

--- PyModel/train_v2.py
+++ PyModel/train_v2.py
@@ -12,7 +12,6 @@ from Transformer.LRSchedule import LRScheduler
 from BaselineTransformer.baselineModel import createBaselineTransformer
 from train_utils import setup_experiment
 import testData as test #For training purposes
-from shared_mem import shared_mem_multiprocessing
 
 if __name__ == "__main__":
     #os.environ["CUDA_VISIBLE_DEVICES"]="1"
@@ -32,16 +31,16 @@ if __name__ == "__main__":
     #Set up experiment
     base_path, p, logger = setup_experiment(args)
 
-    #Set up Dataset
+    #Set up Dataset - different iterations
 
-    #=================================
+    #====================================================================
     # simple scales as a sequence of numbers (no encoding) etc.
-    #=================================
+    #====================================================================
     # train,val,_ = test.mockTfDataset(test.MAJOR_SCALE, 12)
 
-    #=================================
+    #====================================================================
     # A window of {encoder_seq_len} taken throughout the entire midi file
-    #=================================
+    #====================================================================
     # train,val,_ = test.mockTfDataset_from_encoded_midi('./data/processed/MIDI-Unprocessed_01_R1_2008_01-04_ORIG_MID--AUDIO_01_R1_2008_wav--2.midi.pickle', p.encoder_seq_len)
 
     # #Shuffle and Batch data - map for the baseline transformer model
@@ -53,25 +52,34 @@ if __name__ == "__main__":
     # val = val.batch(p.batch_size, drop_remainder=True)
     # val = val.map(test.format_dataset)
 
-    #Instantiate a version of our custom dataset class
-    train = CustomDataset(p, 'train', min_event_length=p.encoder_seq_len*2)
-    for file in train.data:
-      logger.info(f"Using file:{file.path}")
-    train_gen = shared_mem_multiprocessing(train, workers=32)
-    val = CustomDataset(p, 'val', min_event_length=p.encoder_seq_len*2)
+    #====================================================================
+    # Custom dataset class instantiation for train and val
+    #====================================================================
+    train = CustomDataset(p, 'train', min_event_length=p.encoder_seq_len*2,num_files_to_use=1,logger=logger)
+    if train.num_files_to_use != None:
+        logger.info(f"Using only {train.num_files_to_use} files for training")
+        for file in train.data:
+            logger.info(f"Using file:{file.path}")
     
+    #====================================================================
+    #Model / Optimizer Set up
+    #====================================================================
+
     #Instantiate Adam optimizer (NOTE: using the legacy optimizer for running on MacOS CPu)
     optimizer = tf.keras.optimizers.legacy.Adam(LRScheduler(p.model_dim), p.beta_1, p.beta_2, p.epsilon)
 
+    #Choose between custom transformer vs baseline transformer
     if args.with_baseline:
-        print("Creating baseline transformer...")
+        logger.info("Creating baseline transformer...")
         model = createBaselineTransformer(p)
         model.compile(optimizer = optimizer, loss=custom_loss, metrics=[custom_accuracy])
     else:
         model = TransformerModel(p)
         model.compile(optimizer = optimizer, loss_fn = custom_loss, accuracy_fn=custom_accuracy)
 
-    start_time = time()
+    #====================================================================
+    #Callbacks
+    #====================================================================
     model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
         base_path + "checkpoints/checkpoints_{epoch:02d}",
         save_freq='epoch',
@@ -79,23 +87,29 @@ if __name__ == "__main__":
         save_weights_only = True,
     )
     
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss', 
+        patience=3, 
+        restore_best_weights=True
+    )
 
     tensorboard = tf.keras.callbacks.TensorBoard(
         log_dir=base_path+'logs',
         update_freq='epoch',
         histogram_freq = 1,
+        profile_batch='5, 10'
     )
-
+    #====================================================================
     #Train model and save history
+    #====================================================================
+
+    start_time = time()
     try:
         logger.info("Training model...")
         history = model.fit(
             train, 
             epochs = p.epochs,
             callbacks = [model_checkpoint, early_stopping, tensorboard],
-            use_multiprocessing=True,
-            workers = 8
         )
         logger.info("Training Complete! Total time taken: %.2fs" % (time() - start_time))  
         logger.info("Saving History...")
@@ -104,19 +118,3 @@ if __name__ == "__main__":
         logger.info("History Saved!") 
     except Exception as e:
         logger.error(e)
-
-    #Test model against test data set
-    #NOTE : such seq2seq models still need to be evaluated by manually examining output
-    # try:
-    #     logger.info("Testing model...")
-    #     test_data_path = "./data/tf_midi_test_512_1"
-    #     if args.with_baseline:
-    #         test_data=tf.data.Dataset.load(test_data_path+"_baseline")
-    #     else:
-    #         test_data = tf.data.Dataset.load(test_data_path)
-    #     test_data = test_data.shuffle(len(test_data))
-    #     test_data = test_data.batch(p.batch_size, drop_remainder=True)
-    #     test_loss, test_acc = model.evaluate(test_data)
-    #     logger.info("Test Loss: %.4f, Test Accuracy: %.4f" % (test_loss, test_acc))
-    # except Exception as e:
-    #     logger.error(e)
